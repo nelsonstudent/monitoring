f04aa5199e481434af20485cd81d68d938408ea6
Nelson <79551671+nelsonstudent@users.noreply.github.com>
2025-03-11 16:13:06 -0300

    Delete otelcontrib.sh

--- otelcontrib.sh
+++
@@ -1,3 +0,0 @@
-sudo wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.121.0/otelcol-contrib_0.121.0_linux_amd64.deb  
- 
-sudo dpkg -i otelcol-contrib_0.121.0_linux_amd64.deb
